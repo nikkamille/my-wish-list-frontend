80c3ce2f3205881f67fa7302531641660c1a8453
nikkamille <k.pagibigan@gmail.com>
2020-12-24 02:49:42 -0700

    define variables, add event listener

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log("Hi Kuku")
+const wishListForm = document.getElementById("wish-list-form")
+const wishListInput = document.getElementById("wish-list-input")
+
+wishListForm.addEventListener("submit", submitWishList)
+
+function submitWishList(){
+    event.preventDefault()
+    console.log(wishListInput.value)
+}
