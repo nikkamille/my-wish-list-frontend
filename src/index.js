e7f35ac4f291b540989037e2dd63b72895d21ec3
nikkamille <k.pagibigan@gmail.com>
2020-12-24 03:54:37 -0700

    create methods, add event listeners

--- src/index.js
+++ src/index.js
@@ -1,9 +1,32 @@
 const wishListForm = document.getElementById("wish-list-form")
 const wishListInput = document.getElementById("wish-list-input")
+const wishList = document.getElementById("wish-list")
 
 wishListForm.addEventListener("submit", submitWishList)
 
 function submitWishList(){
     event.preventDefault()
-    console.log(wishListInput.value)
+    const li = document.createElement('li')
+
+    const p = document.createElement('p')
+    p.innerText = wishListInput.value
+
+    const itemForm = document.createElement('form')
+    itemForm.innerHTML += `<input type="text" id="itemInput"><input type="submit">`
+    itemForm.addEventListener("submit", submitItem)
+    
+    const itemList = document.createElement('ul')
+
+    li.append(p, itemForm, itemList)
+     
+    wishList.appendChild(li)
+
+    wishListForm.reset()
+
+}
+
+function submitItem(e){
+    e.preventDefault()
+    const itemInput = e.target.children[0].value
+    
 }
