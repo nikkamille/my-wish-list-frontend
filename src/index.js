39b3879eed95ae3f4d7648fabcb44bbaa7144d8d
nikkamille <k.pagibigan@gmail.com>
2020-12-25 22:10:59 -0700

    remove item form from html file; update item form in js file

--- src/index.js
+++ src/index.js
@@ -32,7 +32,15 @@ function displayWishList(){
     p.innerText = wishListInput.value
 
     const itemForm = document.createElement('form')
-    itemForm.innerHTML += `<input type="text" id="item-form"><input type="submit">`
+    itemForm.innerHTML += `<label for="item-name">Item: </label>
+        <input type="text" id="item-name" required><br/>
+        <label for="item-price">Price: </label>
+        <input type="number" id="item-price" required><br/>
+        <label for="item-url">Website: </label>
+        <input type="text" id="item-url" required><br/>
+        <label for="image-url">Image Link: </label>
+        <input type="text" id="image-url"><br/>
+    <input type="submit">`
     itemForm.addEventListener("submit", submitItem)
     
     const itemList = document.createElement('ul')
