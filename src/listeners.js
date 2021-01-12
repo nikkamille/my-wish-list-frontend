5affbe29919c5563430de2c1c0fd5ddf1c4ae255
nikkamille <k.pagibigan@gmail.com>
2021-01-12 03:35:52 -0700

    fix code for event listeners

--- src/listeners.js
+++ src/listeners.js
@@ -10,6 +10,16 @@ document.addEventListener("submit", function(e) {
     let target = e.target
     if(target.matches("#wish-list-form")) {
         e.preventDefault()
+        // let formInput = {
+        //     name: wishListInput.value
+        // }
+        // WishList.submitWishList({wish_list: formInput})
+        //     .then(() => wishListInput.value = "")   
+
+        let formInput = {}
+        target.querySelectorAll("#wish-list-input").forEach(function(wishListInput) {
+            formInput[wishListInput.name] = wishListInput.value
+        })
+        WishList.submitWishList(formInput)
     }
-    
 })
