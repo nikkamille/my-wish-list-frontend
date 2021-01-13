af12bb37e872d1f4b15f55e74ecc442adc06cb92
nikkamille <k.pagibigan@gmail.com>
2021-01-13 08:01:00 -0700

    update renderItem

--- src/items.js
+++ src/items.js
@@ -29,7 +29,41 @@ class Item {
     // </li>
 
     renderItem() {
-        console.log("Working?")
+        this.liElement ||= document.createElement("li")
+        this.liElement.classList.add(..."grid-cols-12 border-2 rounded-md p-2 my-4".split(" "))
+
+        const br = document.createElement("BR")
+
+        this.image ||= document.createElement("img")
+        this.image.src = this.image_url
+        this.image.alt = this.name
+        this.image.width = 600
+        this.image.height = 400
+        this.image.classList.add(..."mx-auto place-content-center m-2".split(" "))
+
+        this.itemName ||= document.createElement("p")
+        this.itemName.classList.add(..."text-center font-medium".split(" "))
+        this.itemName.textContent = this.name
+
+        this.itemPrice ||= document.createElement("p")
+        this.itemPrice.classList.add(..."text-center font-medium -m-6".split(" "))
+        this.itemPrice.textContent = "$" + this.price 
+
+        this.itemLink ||= document.createElement("a")
+        this.itemLink.href = this.url
+        this.itemLink.classList.add(..."text-center font-medium text-blue-700 break-all".split(" "))
+
+        this.itemDeleteButton ||= document.createElement("button")
+        this.itemDeleteButton.classList.add(..."text-sm font-bold text-red-700 focus:outline-none".split(" "))
+        this.itemDeleteButton.innerHTML = "DELETE"
+
+        this.liElement.append(this.image, this.itemName, this.itemPrice, this.itemLink, this.itemDeleteButton)
+        this.liElement.insertBefore(br, this.itemPrice)
+        // this.liElement.insertBefore(br, this.itemDeleteButton)
+        this.constructor.itemContainer().appendChild(this.liElement)
+
+
+        return this.liElement
     }
 
   
